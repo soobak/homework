d82819e35e7137980ee7899582927854ed83c260
soobak <jnam08@gmail.com>
2015-02-25 01:21:40 -0500

    added initial values

--- hw5.py
+++ hw5.py
@@ -2,3 +2,13 @@
 # jnam08@gatech.edu
 # My partner, Brett Davis, and I worked on this assignment alone, using only this semester's course materials.
 
+from Graphics import *
+from Myro import timer #keeps track of timer
+
+NUMBALLS = 1
+WINSIZE = 500
+win = Window("Pong", WINSIZE, WINSIZE)
+win.mode = "manual" # to make it look "smoother"
+balls = []
+dx = []
+dy = []
